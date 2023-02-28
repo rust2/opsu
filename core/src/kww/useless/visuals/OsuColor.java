c7a9acc98a35d0fff97055a9a0586666f894b06f
rust2 <rustchannelz@gmail.com>
2023-03-01 01:06:58 +0500

    colors

---
+++ core/src/kww/useless/visuals/OsuColor.java
@@ -0,0 +1,176 @@
+package kww.useless.visuals;
+
+import kww.useless.stub.ScoreRank;
+
+public class OsuColor {
+    /* Everything below is from lazer */
+    public static final Color255 PurpleLighter = new Color255().fromHex("eeeeff");
+    public static final Color255 PurpleLight = new Color255().fromHex("aa88ff");
+    public static final Color255 PurpleLightAlternative = new Color255().fromHex("cba4da");
+    public static final Color255 Purple = new Color255().fromHex("8866ee");
+    public static final Color255 PurpleDark = new Color255().fromHex("6644cc");
+    public static final Color255 PurpleDarkAlternative = new Color255().fromHex("312436");
+    public static final Color255 PurpleDarker = new Color255().fromHex("441188");
+
+    public static final Color255 PinkLighter = new Color255().fromHex("ffddee");
+    public static final Color255 PinkLight = new Color255().fromHex("ff99cc");
+    public static final Color255 Pink = new Color255().fromHex("ff66aa");
+    public static final Color255 PinkDark = new Color255().fromHex("cc5288");
+    public static final Color255 PinkDarker = new Color255().fromHex("bb1177");
+
+    public static final Color255 BlueLighter = new Color255().fromHex("ddffff");
+    public static final Color255 BlueLight = new Color255().fromHex("99eeff");
+    public static final Color255 Blue = new Color255().fromHex("66ccff");
+    public static final Color255 BlueDark = new Color255().fromHex("44aadd");
+    public static final Color255 BlueDarker = new Color255().fromHex("2299bb");
+
+    public static final Color255 YellowLighter = new Color255().fromHex("ffffdd");
+    public static final Color255 YellowLight = new Color255().fromHex("ffdd55");
+    public static final Color255 Yellow = new Color255().fromHex("ffcc22");
+    public static final Color255 YellowDark = new Color255().fromHex("eeaa00");
+    public static final Color255 YellowDarker = new Color255().fromHex("cc6600");
+
+    public static final Color255 GreenLighter = new Color255().fromHex("eeffcc");
+    public static final Color255 GreenLight = new Color255().fromHex("b3d944");
+    public static final Color255 Green = new Color255().fromHex("88b300");
+    public static final Color255 GreenDark = new Color255().fromHex("668800");
+    public static final Color255 GreenDarker = new Color255().fromHex("445500");
+
+    public static final Color255 Sky = new Color255().fromHex("6bb5ff");
+    public static final Color255 GreySkyLighter = new Color255().fromHex("c6e3f4");
+    public static final Color255 GreySkyLight = new Color255().fromHex("8ab3cc");
+    public static final Color255 GreySky = new Color255().fromHex("405461");
+    public static final Color255 GreySkyDark = new Color255().fromHex("303d47");
+    public static final Color255 GreySkyDarker = new Color255().fromHex("21272c");
+
+    public static final Color255 SeaFoam = new Color255().fromHex("05ffa2");
+    public static final Color255 GreySeaFoamLighter = new Color255().fromHex("9ebab1");
+    public static final Color255 GreySeaFoamLight = new Color255().fromHex("4d7365");
+    public static final Color255 GreySeaFoam = new Color255().fromHex("33413c");
+    public static final Color255 GreySeaFoamDark = new Color255().fromHex("2c3532");
+    public static final Color255 GreySeaFoamDarker = new Color255().fromHex("1e2422");
+
+    public static final Color255 Cyan = new Color255().fromHex("05f4fd");
+    public static final Color255 GreyCyanLighter = new Color255().fromHex("77b1b3");
+    public static final Color255 GreyCyanLight = new Color255().fromHex("436d6f");
+    public static final Color255 GreyCyan = new Color255().fromHex("293d3e");
+    public static final Color255 GreyCyanDark = new Color255().fromHex("243536");
+    public static final Color255 GreyCyanDarker = new Color255().fromHex("1e2929");
+
+    public static final Color255 Lime = new Color255().fromHex("82ff05");
+    public static final Color255 GreyLimeLighter = new Color255().fromHex("deff87");
+    public static final Color255 GreyLimeLight = (Color255) new Color255().fromHex("657259");
+    public static final Color255 GreyLime = (Color255) new Color255().fromHex("3f443a");
+    public static final Color255 GreyLimeDark = new Color255().fromHex("32352e");
+    public static final Color255 GreyLimeDarker = new Color255().fromHex("2e302b");
+
+    public static final Color255 Violet = new Color255().fromHex("bf04ff");
+    public static final Color255 GreyVioletLighter = new Color255().fromHex("ebb8fe");
+    public static final Color255 GreyVioletLight = new Color255().fromHex("685370");
+    public static final Color255 GreyViolet = new Color255().fromHex("46334d");
+    public static final Color255 GreyVioletDark = new Color255().fromHex("2c2230");
+    public static final Color255 GreyVioletDarker = new Color255().fromHex("201823");
+
+    public static final Color255 Carmine = new Color255().fromHex("ff0542");
+    public static final Color255 GreyCarmineLighter = new Color255().fromHex("deaab4");
+    public static final Color255 GreyCarmineLight = new Color255().fromHex("644f53");
+    public static final Color255 GreyCarmine = new Color255().fromHex("342b2d");
+    public static final Color255 GreyCarmineDark = new Color255().fromHex("302a2b");
+    public static final Color255 GreyCarmineDarker = new Color255().fromHex("241d1e");
+
+    public static final Color255 Gray0 = new Color255().fromHex("000");
+    public static final Color255 Gray1 = new Color255().fromHex("111");
+    public static final Color255 Gray2 = new Color255().fromHex("222");
+    public static final Color255 Gray3 = new Color255().fromHex("333");
+    public static final Color255 Gray4 = new Color255().fromHex("444");
+    public static final Color255 Gray5 = new Color255().fromHex("555");
+    public static final Color255 Gray6 = new Color255().fromHex("666");
+    public static final Color255 Gray7 = new Color255().fromHex("777");
+    public static final Color255 Gray8 = new Color255().fromHex("888");
+    public static final Color255 Gray9 = new Color255().fromHex("999");
+    public static final Color255 GrayA = new Color255().fromHex("aaa");
+    public static final Color255 GrayB = new Color255().fromHex("bbb");
+    public static final Color255 GrayC = new Color255().fromHex("ccc");
+    public static final Color255 GrayD = new Color255().fromHex("ddd");
+    public static final Color255 GrayE = new Color255().fromHex("eee");
+    public static final Color255 GrayF = new Color255().fromHex("fff");
+
+    /////////////////////
+
+    public static final Color255 Pink0 = new Color255().fromHex("ff99c7");
+    public static final Color255 Pink1 = new Color255().fromHex("ff66ab");
+    public static final Color255 Pink2 = new Color255().fromHex("eb4791");
+    public static final Color255 Pink3 = new Color255().fromHex("cc3378");
+    public static final Color255 Pink4 = new Color255().fromHex("6b2e49");
+
+    public static final Color255 Purple0 = new Color255().fromHex("b299ff");
+    public static final Color255 Purple1 = new Color255().fromHex("8c66ff");
+    public static final Color255 Purple2 = new Color255().fromHex("7047eb");
+    public static final Color255 Purple3 = new Color255().fromHex("5933cc");
+    public static final Color255 Purple4 = new Color255().fromHex("3d2e6b");
+
+    public static final Color255 Blue0 = new Color255().fromHex("99ddff");
+    public static final Color255 Blue1 = new Color255().fromHex("66ccff");
+    public static final Color255 Blue2 = new Color255().fromHex("47b4eb");
+    public static final Color255 Blue3 = new Color255().fromHex("3399cc");
+    public static final Color255 Blue4 = new Color255().fromHex("2e576b");
+
+    public static final Color255 Green0 = new Color255().fromHex("99ffa2");
+    public static final Color255 Green1 = new Color255().fromHex("66ff73");
+    public static final Color255 Green2 = new Color255().fromHex("47eb55");
+    public static final Color255 Green3 = new Color255().fromHex("33cc40");
+    public static final Color255 Green4 = new Color255().fromHex("2e6b33");
+
+    public static final Color255 Lime0 = new Color255().fromHex("ccff99");
+    public static final Color255 Lime1 = new Color255().fromHex("b2ff66");
+    public static final Color255 Lime2 = new Color255().fromHex("99eb47");
+    public static final Color255 Lime3 = new Color255().fromHex("7fcc33");
+    public static final Color255 Lime4 = new Color255().fromHex("4c6b2e");
+
+    public static final Color255 Orange0 = new Color255().fromHex("ffe699");
+    public static final Color255 Orange1 = new Color255().fromHex("ffd966");
+    public static final Color255 Orange2 = new Color255().fromHex("ebc247");
+    public static final Color255 Orange3 = new Color255().fromHex("cca633");
+    public static final Color255 Orange4 = new Color255().fromHex("6b5c2e");
+
+    public static final Color255 Red0 = new Color255().fromHex("ff9b9b");
+    public static final Color255 Red1 = new Color255().fromHex("ff6666");
+    public static final Color255 Red2 = new Color255().fromHex("eb4747");
+    public static final Color255 Red3 = new Color255().fromHex("cc3333");
+    public static final Color255 Red4 = new Color255().fromHex("6b2e2e");
+
+    ///////////////////
+
+    public static final Color255 X = new Color255().fromHex("de31ae");
+    public static final Color255 S = new Color255().fromHex("02b5c3");
+    public static final Color255 A = new Color255().fromHex("88da20");
+    public static final Color255 B = new Color255().fromHex("e3b130");
+    public static final Color255 C = new Color255().fromHex("ff8e5d");
+    public static final Color255 D = new Color255().fromHex("ff5a5a");
+
+    public static Color255 ForRank(ScoreRank rank)
+    {
+        switch (rank)
+        {
+            case XH:
+            case X:
+                return X;
+
+            case SH:
+            case S:
+                return S;
+
+            case A:
+                return A;
+
+            case B:
+                return B;
+
+            case C:
+                return C;
+
+            default:
+                return D;
+        }
+    }
+}
